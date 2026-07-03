64a7d014ed51ece4d06b62726b65b7b5dcee55cf
Jonah Rosner <rosner1@purdue.edu>
2026-07-02 21:33:22 -0400

    Initial commit

---
+++ auth_system/src/test/java/jonah_rosner/auth_system/AuthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package jonah_rosner.auth_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
